29497bb46b5dc5427b3e8ec8e83d699f0fe0e087
Nguyen Dinh Hoang Huy <0788909221ndhh@gmail.com>
2024-05-22 16:29:03 +0700

    CourtDetailDAO

---
+++ Badminton.Data/DAO/CourtDetailDAO.cs
@@ -0,0 +1,17 @@
+﻿using Badminton.Data.Base;
+using Badminton.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Badminton.Data.DAO
+{
+    public class CourtDetailDAO : BaseDAO<CourtDetail>
+    {
+        public CourtDetailDAO()
+        {
+        }
+    }
+}

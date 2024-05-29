bca832ad666f0b38b82c5926406575a01883304a
KienPC115 <letrungkien2463@gmail.com>
2024-05-29 15:47:04 +0700

    Add savechangeTransaction

---
+++ Badminton.RazorWebApp/Pages/Court.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Badminton.RazorWebApp.Pages
+{
+    public class CourtModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

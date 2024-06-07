433ff06558f6e7550e1db4f5e8559586ef7af622
Nguyen Dinh Hoang Huy <0788909221ndhh@gmail.com>
2024-06-07 13:30:28 +0700

    Add Shared for CourtDetail & Court & Order

---
+++ Badminton.Business/Shared/OrderShared.cs
@@ -0,0 +1,10 @@
+﻿namespace Badminton.Business.Shared;
+
+public static class OrderShared
+{
+    public static List<string> Type()
+    {
+        string[] Type = { "Paid", "UnPaid" };
+        return new List<string>(Type);
+    }
+}

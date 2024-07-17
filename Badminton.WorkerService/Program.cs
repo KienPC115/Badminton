a2d5d85ade98c8e80e9a3e1eea4632efc1a63f9a
MinhKQ <minhkieu702@gmail.com>
2024-07-18 00:10:27 +0700

    update search

--- Badminton.WorkerService/Program.cs
+++ Badminton.WorkerService/Program.cs
@@ -1,10 +1,23 @@
 using Badminton.Business;
+using Badminton.Data.Models;
 using Badminton.WorkerService;
+using Microsoft.EntityFrameworkCore;
 
 var builder = Host.CreateDefaultBuilder(args)
     .ConfigureServices((hostContext, services) =>
     {
         services.AddHostedService<Worker>();
+        services.AddDbContext<Net1710_221_8_BadmintonContext>(options =>
+            options.UseSqlServer(
+                hostContext.Configuration.GetConnectionString("DefaultConnection"),
+                sqlServerOptionsAction: sqlOptions =>
+                {
+                    sqlOptions.EnableRetryOnFailure(
+                        maxRetryCount: 5, // Maximum number of retry attempts (default is 6)
+                        maxRetryDelay: TimeSpan.FromSeconds(30), // Maximum delay between retries
+                        errorNumbersToAdd: null // List of additional error numbers to retry on
+                    );
+                }));
     })
     .UseWindowsService();
 
--- Badminton.WorkerService/Worker.cs
+++ Badminton.WorkerService/Worker.cs
@@ -1,4 +1,6 @@
 using Badminton.Business;
+using System.Runtime.Loader;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Badminton.WorkerService
 {
@@ -6,7 +8,7 @@ namespace Badminton.WorkerService
     {
         private readonly ILogger<Worker> _logger;
         private readonly ICourtDetailBusiness _courtDetailBusiness;
-
+        private readonly string FilePath = "D:\\now_semester\\PRN221\\Net1710_221_8_Goodminton\\Source\\Badminton\\Badminton.WorkerService\\Log.txt";
         public Worker(ILogger<Worker> logger)
         {
             _courtDetailBusiness ??= new CourtDetailBusiness();
@@ -16,40 +18,18 @@ namespace Badminton.WorkerService
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             #region Refresh Status After Next 3s
-            //while (!stoppingToken.IsCancellationRequested)
-            //{
-            //    if (_logger.IsEnabled(LogLevel.Information))
-            //    {
-            //        _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            //        var result = await _courtDetailBusiness.RefreshCourtDetailStatus();
-            //        _logger.LogInformation($"Result of Refresh Court Detail Status: {result.Status} - {result.Message}");
-            //    }
-            //    await Task.Delay(3000, stoppingToken);
-            //}
-            #endregion
-            #region Refresh Status When time at 00:00:00
             while (!stoppingToken.IsCancellationRequested)
             {
-                var now = DateTime.Now;
-                var nextRun = DateTime.Today.AddDays(1); //00:00:00 is tomorrow
-                var delay = nextRun - now;
-
-                _logger.LogInformation($"Current time: {now}, next run at: {nextRun}, delay: {delay}");
-
-                if (delay.TotalMilliseconds > 0)
+                if (_logger.IsEnabled(LogLevel.Information))
                 {
-                    await Task.Delay(delay, stoppingToken);
-                }
-                try
-                {
+                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                     var result = await _courtDetailBusiness.RefreshCourtDetailStatus();
-                    _logger.LogInformation($"Result of Refresh Court Detail Status: {result.Status} - {result.Message.ToString()}");
+                    var text = $"Result of Refresh Court Detail Status: {result.Status} - {result.Message}";
+                    _logger.LogInformation(text);
+                    await File.AppendAllTextAsync(FilePath, text+"\n\n");
+
                 }
-                catch (Exception ex)
-                {
-                    _logger.LogError(ex, "An error occurred while refreshing court detail status");
-                }
-                await Task.Delay(TimeSpan.FromDays(1), stoppingToken);
+                await Task.Delay(3000, stoppingToken);
             }
             #endregion
         }

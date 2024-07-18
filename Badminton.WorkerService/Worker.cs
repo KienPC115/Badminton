5c7049ce1393d31f86728742c9301d1faa604698
MinhKQ <minhkieu702@gmail.com>
2024-07-18 12:36:05 +0700

    Update Worker.cs

--- Badminton.WorkerService/Worker.cs
+++ Badminton.WorkerService/Worker.cs
@@ -22,17 +22,42 @@ namespace Badminton.WorkerService
             try
             {
                 #region Refresh Status After Next 3s
+                //while (!stoppingToken.IsCancellationRequested)
+                //{
+                //    if (_logger.IsEnabled(LogLevel.Information))
+                //    {
+                //        _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                //        var result = await _courtDetailBusiness.RefreshCourtDetailStatus();
+                //        var text = $"{DateTime.Now} Result of Refresh Court Detail Status: {result.Status} - {result.Message}";
+                //        _logger.LogInformation(text);
+                //        await File.AppendAllTextAsync(FilePath, text + "\n\n");
+                //    }
+                //    await Task.Delay(3000, stoppingToken);
+                //}
+                #endregion
+                #region Refresh Status When time at 00:00:00
                 while (!stoppingToken.IsCancellationRequested)
                 {
-                    if (_logger.IsEnabled(LogLevel.Information))
+                    var now = DateTime.Now;
+                    var nextRun = DateTime.Today.AddDays(1); //00:00:00 is tomorrow
+                    var delay = nextRun - now;
+
+                    _logger.LogInformation($"Current time: {now}, next run at: {nextRun}, delay: {delay}");
+
+                    if (delay.TotalMilliseconds > 0)
                     {
-                        _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                        var result = await _courtDetailBusiness.RefreshCourtDetailStatus();
-                        var text = $"{DateTime.Now} Result of Refresh Court Detail Status: {result.Status} - {result.Message}";
-                        _logger.LogInformation(text);
-                        await File.AppendAllTextAsync(FilePath, text + "\n\n");
+                        await Task.Delay(delay, stoppingToken);
                     }
-                    await Task.Delay(3000, stoppingToken);
+                    try
+                    {
+                        var result = await _courtDetailBusiness.RefreshCourtDetailStatus();
+                        _logger.LogInformation($"Result of Refresh Court Detail Status: {result.Status} - {result.Message.ToString()}");
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError(ex, "An error occurred while refreshing court detail status");
+                    }
+                    await Task.Delay(TimeSpan.FromDays(1), stoppingToken);
                 }
                 #endregion
             }
